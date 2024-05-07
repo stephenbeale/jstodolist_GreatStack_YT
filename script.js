04d4a5ce282052a8dd9bcf891f25b104b32a4335
Steve Beale <s.j.beale@swansea.ac.uk>
2024-05-07 11:17:30 +0100

    WIP - tick boxes and cross icons

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value == '') {
+        alert("You must write something!");
+    }
+    else{
+        /*Variable assignment for a new string*/
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li)
+        let span = document.createElement("span");
+        /**Cross icon*/
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    /**clears the box after entry*/
+    inputBox.value = "";
+}
